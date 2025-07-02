e3e70dbb8e31d891202acbca1b96e47a3b5bd242
Abhay Sahany <abhaysahany1982@gmail.com>
2025-07-02 11:27:46 +0530

    new

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@ $id = $_GET['id'];
 $query = "DELETE FROM `users` WHERE `id`= $id";
 $run = mysqli_query($con,$query);
 if ($run) {
-    echo "Data deleted successfully";
+    echo "Data deleted successfully to database";
    
     header('location:read.php');
 } else {
